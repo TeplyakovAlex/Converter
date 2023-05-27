6dc02d9c32dc959602afa5e8357372eccabf7147
admin <teplyakov.in@gmail.com>
2023-05-27 21:54:16 +0200

    Add version info

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter for base converting");
-
+        System.out.println("Version 1.0");
         double miles = 15;
         double kms = 26;
         double kmsRes = convertMilesToKms(miles);
